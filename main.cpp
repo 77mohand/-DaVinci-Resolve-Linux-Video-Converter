25db865097c4c8b825bbbeb03eb4433f6759559b
Mohand Amr <mohandpro50@gmail.com>
2026-07-11 20:43:58 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <filesystem>
+using namespace std;
+namespace fs = std::filesystem;
+
+int main()
+{
+    string input;
+    cout<<"Please enter your file path" << endl;
+    getline(cin,input);
+    fs::path inputPath(input);
+
+    string folder = inputPath.parent_path().string();
+    string filename = inputPath.stem().string();
+
+    cout << "The folder containing the video is: " << folder << endl;
+    cout<< "The video's name without an extension is: " << filename <<endl;
+
+    string output = folder + "/" + filename + "_davinci.mov";
+
+    string command = "ffmpeg -i \"" + input + "\" -c:v dnxhd -profile:v dnxhr_hq -c:a pcm_s16le -y \"" + output + "\"";
+    cout<<command<<endl;
+
+    system(command.c_str());
+
+}
