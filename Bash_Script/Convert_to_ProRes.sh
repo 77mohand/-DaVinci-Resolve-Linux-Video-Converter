df29f282240b662e28d67227b71481e2376ef418
Mohand Amr <mohandpro50@gmail.com>
2026-07-14 08:50:48 +0300

    update for bath script

--- Bash_Script/Convert_to_ProRes.sh
+++ Bash_Script/Convert_to_ProRes.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
-# التحقق من أن المستخدم حدد ملفاً
 if [ -z "$1" ]; then
-    notify-send "خطأ" "لم يتم تحديد أي ملف فيديو"
+    notify-send "Error" "No video file selected"
     exit 1
 fi
 
 FILE="$1"
 OUTPUT="${FILE%.*}_prores.mov"
 
-# التحويل إلى ProRes LT (profile=1) وصوت PCM غير مضغوط
 ffmpeg -i "$FILE" -c:v prores -profile:v 0 -c:a pcm_s16le "$OUTPUT"
 
-# إرسال إشعار للنظام عند الانتهاء
-notify-send "اكتمل التحويل" "تم تحويل الفيديو إلى ProRes بنجاح وجاهز لداڤينشي!"
+notify-send "Conversion complete" "Video successfully converted to ProRes and ready for DaVinci!"
 
